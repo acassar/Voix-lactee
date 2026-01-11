f42a1b2282138c2822dc16444cdeffca13191215
acassar <adricass@live.fr>
2026-01-11 14:32:33 +0100

    home page and removed strappi example

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      name: 'home',
+      path: '/',
+      component: () => import('@/components/home/HomeComponent.vue'),
+    },
+  ],
 })
 
 export default router
