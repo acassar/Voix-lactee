7a08107eeae123d33db9ac8bb9ecdae9834d177e
acassar <adricass@live.fr>
2026-01-17 22:44:14 +0100

    created fetcher base

---
+++ front/src/domain/api/Fetcher.ts
@@ -0,0 +1,13 @@
+export interface Fetcher {
+  get: (url: string, options?: RequestInit) => Promise<Response>
+}
+
+export const fetcher: Fetcher = {
+  get: async (url: string, options: RequestInit = {}) => {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}${url}`, {
+      method: 'GET',
+      ...options,
+    })
+    return response
+  },
+}
