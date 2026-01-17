68146ecab1b1643992a32ce76eeebe9d71f80cdc
acassar <adricass@live.fr>
2026-01-17 19:19:14 +0100

    hero text and img

--- front/src/components/home/TopHero.vue
+++ front/src/components/home/TopHero.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="flex">
-    <div id="home-title" class="p-14">
+    <div id="home-title" class="xl:pl-100 xl:pr- lg:pl-60 md:pl-40 p-14">
       <h1 class="text-4xl h1 font-bold mb-4">Voix Lactée</h1>
       <p class="text-lg text-white">
         Bienvenue dans cet espace dédié à la parentalité et au lien avec votre bébé. Je vous
@@ -11,9 +11,18 @@
       </p>
     </div>
     <div id="home-hero-img">
-      <!-- <img src="@/assets/images/hero-baby.jpg" alt="Bébé" class="w-full h-auto object-cover" /> -->
+      <span class="hero-img"
+        ><img src="@/assets/images/portage_bebe.jpg" alt="Bébé" class="w-full h-auto"
+      /></span>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.hero-img img {
+  border-radius: 170rem 0 0 230rem;
+  width: 1000px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  height: 100%;
+}
+</style>
