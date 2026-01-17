6a06d1671f604f2b31cf35d558167b22bd75c397
acassar <adricass@live.fr>
2026-01-17 21:53:07 +0100

    added waves to split home sections

---
+++ front/src/components/common/style/WaveComponent.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+const props = defineProps<{
+  color: string
+  rotate?: boolean
+}>()
+</script>
+
+<template>
+  <div class="wave" v-bind="props">
+    <svg viewBox="0 0 1440 100" preserveAspectRatio="none">
+      <path
+        d="M0,40 C120,80 320,0 520,20 720,40 920,80 1120,50 1320,20 1440,40 1440,40 L1440,0 L0,0 Z"
+        :fill="color"
+      />
+    </svg>
+  </div>
+</template>
+
+<style scoped>
+.wave > svg {
+  display: block;
+  width: 100%;
+}
+
+.wave[rotate='true'] {
+  transform: rotate(180deg);
+  margin: -1px 0;
+}
+</style>
--- front/src/views/HomePage.vue
+++ front/src/views/HomePage.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import WaveComponent from '@/components/common/style/WaveComponent.vue'
 import FAQComponent from '@/components/home/FAQComponent.vue'
 import HomeServices from '@/components/home/HomeServices.vue'
 import TopHero from '@/components/home/TopHero.vue'
@@ -7,7 +8,9 @@ import TopHero from '@/components/home/TopHero.vue'
 <template>
   <div>
     <TopHero />
+    <WaveComponent color="var(--color-secondary)" rotate></WaveComponent>
     <HomeServices />
+    <WaveComponent color="var(--color-secondary)"></WaveComponent>
     <FAQComponent />
   </div>
 </template>
